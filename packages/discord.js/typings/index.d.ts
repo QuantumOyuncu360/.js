3584cdeba1e79acf836fb31c0a66961818752ae3
TÆMBØ <kuneshr123@gmail.com>
2024-06-21 21:18:05 -0700

    Add types for using PollBuilder in message payload

--- packages/discord.js/typings/index.d.ts
+++ packages/discord.js/typings/index.d.ts
@@ -192,6 +192,7 @@ import {
   GuildScheduledEventRecurrenceRuleMonth,
   GuildScheduledEventRecurrenceRuleFrequency,
   APIPollMedia,
+  RESTAPIPollCreate,
 } from 'discord-api-types/v10';
 import { ChildProcess } from 'node:child_process';
 import { EventEmitter } from 'node:events';
@@ -6541,7 +6542,7 @@ export interface BaseMessageOptions {
 }
 
 export interface BaseMessageOptionsWithPoll extends BaseMessageOptions {
-  poll?: PollData;
+  poll?: JSONEncodable<RESTAPIPollCreate> | PollData;
 }
 
 export interface MessageCreateOptions extends BaseMessageOptionsWithPoll {
